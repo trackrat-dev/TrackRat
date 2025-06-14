9749bebdf005d286d00b1dd9493076a4bd1b9086
Andy <>
2025-06-14 10:45:51 -0400

    minor secret sensitive fix

--- infra/modules/secrets/outputs.tf
+++ infra/modules/secrets/outputs.tf
@@ -11,14 +11,17 @@ output "secret_id" {
 output "db_password_secret_id" {
   description = "The secret_id of the database password secret in Secret Manager."
   value       = var.db_password_plaintext != null ? google_secret_manager_secret.db_password[0].secret_id : null
+  sensitive   = true
 }
 
 output "db_password_secret_version_name" {
   description = "The resource name of the latest version of the database password secret."
   value       = var.db_password_plaintext != null ? google_secret_manager_secret_version.db_password_version[0].name : null
+  sensitive   = true
 }
 
 output "db_password_secret_name" {
   description = "The full resource name of the database password secret."
   value       = var.db_password_plaintext != null ? google_secret_manager_secret.db_password[0].name : null
+  sensitive   = true
 }
