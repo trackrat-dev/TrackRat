9643a72d92d8de8816e75096fd4722ad194a3e8a
Andy <>
2025-08-30 13:19:08 -0400

    Fix tests

--- backend_v2/src/trackrat/db/migrations/versions/20250830_1258-6f98f8469447_change_cle_station_code_to_csn_for_.py
+++ backend_v2/src/trackrat/db/migrations/versions/20250830_1258-6f98f8469447_change_cle_station_code_to_csn_for_.py
@@ -5,13 +5,12 @@ Revises: 2d9bbf285b25
 Create Date: 2025-08-30 12:58:02.242819
 
 """
-from alembic import op
-import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '6f98f8469447'
-down_revision = '2d9bbf285b25'
+revision = "6f98f8469447"
+down_revision = "2d9bbf285b25"
 branch_labels = None
 depends_on = None
 
@@ -19,10 +18,14 @@ depends_on = None
 def upgrade() -> None:
     """Apply migration."""
     # Update all CLE station codes to CSN for Clemson
-    op.execute("UPDATE journey_stops SET station_code = 'CSN' WHERE station_code = 'CLE'")
+    op.execute(
+        "UPDATE journey_stops SET station_code = 'CSN' WHERE station_code = 'CLE'"
+    )
 
 
 def downgrade() -> None:
     """Revert migration."""
     # Revert CSN station codes back to CLE
-    op.execute("UPDATE journey_stops SET station_code = 'CLE' WHERE station_code = 'CSN'")
+    op.execute(
+        "UPDATE journey_stops SET station_code = 'CLE' WHERE station_code = 'CSN'"
+    )
